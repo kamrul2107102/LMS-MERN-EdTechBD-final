047cf6e4e8a645e0983618af590302a3f017740a
kamrul2107102 <ki021cp@gmail.com>
2025-09-30 21:52:34 +0600

    working version

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -369,7 +369,7 @@ export default function CourseDetails() {
                 onClick={enrollCourse}
                 className="px-8 py-2 rounded-md bg-blue-600 text-white font-semibold text-sm md:text-base shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-400 mx-auto block"
               >
-                {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+                {isAlreadyEnrolled ? "Already Enrolled" : "Enroll ..."}
               </button>
             </div>
 
